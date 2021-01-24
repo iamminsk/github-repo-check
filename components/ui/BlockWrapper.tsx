6da21ab3295b4bf17985dcefa74d208f3925eba8
ian <ian@macbook-pro.home>
2021-01-24 18:40:32 +0100

    add BlockWrapper and Button components

---
+++ components/ui/BlockWrapper.tsx
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { forwardRef } from "react";
+import { Interpolation, Theme, jsx } from "@emotion/react";
+
+interface BlockWrapperProps {
+  wrapperCss?: Interpolation<Theme>;
+  as?: keyof JSX.IntrinsicElements;
+}
+
+export const BlockWrapper = forwardRef<
+  HTMLDivElement,
+  BlockWrapperProps & JSX.IntrinsicElements["div"]
+>(({ as: Element = "section", children, wrapperCss, ...rest }, ref) => {
+  return (
+    <Element css={wrapperCss}>
+      <div
+        ref={ref}
+        css={{
+          maxWidth: 1240,
+          margin: "0 auto",
+          padding: "0 30px",
+          height: "100%",
+        }}
+        {...rest}
+      >
+        {children}
+      </div>
+    </Element>
+  );
+});
