101ed053e4cbcafc319c810070453a67a93eb110
ian <ian@MacBook-Pro.local>
2021-01-26 23:10:10 +0100

    modify Creator and Summary components

--- components/Creator/index.tsx
+++ components/Creator/index.tsx
@@ -1,14 +1,68 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { HexColorPicker } from "react-colorful";
+import "react-colorful/dist/index.css";
 
-export const Creator = ({ setCurrentStep }) => {
-  const onClick = React.useCallback(() => {
+import { Input } from "../ui/Input";
+
+export const Creator = ({ setCurrentStep, setFormData }) => {
+  const form = useForm({
+    reValidateMode: "onSubmit",
+  });
+  const { register, handleSubmit, errors, getValues } = form;
+
+  const onFormSubmit = React.useCallback((data) => {
+    setFormData({
+      user: getValues("name"),
+      repoName: getValues("repoName"),
+      bgColor: encodeURIComponent(bgColor),
+    });
     setCurrentStep("summary");
   }, []);
+
+  const [bgColor, setBgColor] = React.useState("#b32aa9");
 
   return (
     <section>
       <p>Creator</p>
-      <button onClick={onClick}>get url</button>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <Input
+          label="user name"
+          name="name"
+          ref={register({
+            required: "required field",
+            validate: {
+              exists: async (value) => {
+                const res = await fetch(
+                  `https://api.github.com/users/${value}`
+                );
+                return res.ok || "user doesn't exist";
+              },
+            },
+          })}
+          errorMessage={errors.name?.message}
+          wrapperCss={{ marginBottom: 10 }}
+        />
+        <Input
+          label="repo name"
+          name="repoName"
+          ref={register({
+            required: "required field",
+            validate: {
+              exists: async (value) => {
+                const res = await fetch(
+                  `https://api.github.com/repos/${getValues("name")}/${value}`
+                );
+                return res.ok || "repo doesn't exist";
+              },
+            },
+          })}
+          errorMessage={errors.repoName?.message}
+          wrapperCss={{ marginBottom: 10 }}
+        />
+        <HexColorPicker color={bgColor} onChange={setBgColor} />
+        <button type="submit">get url</button>
+      </form>
     </section>
   );
 };
--- components/Summary/index.tsx
+++ components/Summary/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
-export const Summary = ({ setCurrentStep }) => {
+export const Summary = ({ setCurrentStep, formData }) => {
   const onClick = React.useCallback(() => {
     setCurrentStep("creator");
   }, []);
 
+  const { user, repoName, bgColor } = formData;
+  const domain = process.env.VERCEL_URL || "localhost:3000";
+  const url = `/${user}/${repoName}?bgColor=${bgColor}`;
+
   return (
     <section>
       <p>Summary</p>
+      <h2>Your repo summary is available under:</h2>
+      <Link href={url}>
+        <a>{`https://${domain + url}`}</a>
+      </Link>
+      <p>{formData.bgColor}</p>
+      <p>{formData.user}</p>
+      <p>{formData.repoName}</p>
       <button onClick={onClick}>create new</button>
     </section>
   );
